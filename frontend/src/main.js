61c695f31da17a8d2a5e910c88661c40855e6d0c
kookhh0827 <kookhh0827@gmail.com>
2021-10-12 14:08:52 +0000

    sorry for confusion: use 'store' in main.js and use 'export default new Vuex.Store({'

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
-import {store} from './store';
+import store from './store';
 
 Vue.config.productionTip = false;
 Vue.prototype.axios = axios;
