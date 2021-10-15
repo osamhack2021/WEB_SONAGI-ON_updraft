d2dc45bf6413527f1d7ee7d64c73953098325a15
kookhh0827 <kookhh0827@gmail.com>
2021-10-15 09:48:14 +0000

    resolve some errors

--- sonagi/usersetting/serializers.py
+++ sonagi/usersetting/serializers.py
@@ -7,7 +7,7 @@ class UserSettingSerializer(serializers.ModelSerializer):
         model = UserSetting
         fields = ["email", "nickname", "major", "type", "enlisted_date", "delisted_date", "promotion1_date", "promotion2_date", "promotion3_date"]
         extra_kwargs = {"email": {"read_only": True}}
-    def validate_nickname(val):
+    def validate_nickname(self, val):
         if len(val) < 4 or len(val) > 15 :
             raise serializers.ValidationError({'nickname' : '닉네임은 4자 이상 15자 이하여야 합니다.'})
         return val
