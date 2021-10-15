56ae0dc6570fb22e5803e06269074e6ab1109ff1
kookhh0827 <kookhh0827@gmail.com>
2021-10-15 09:22:57 +0000

    add email in usersetting serializer(read-only)

--- sonagi/usersetting/serializers.py
+++ sonagi/usersetting/serializers.py
@@ -5,5 +5,10 @@ from rest_framework import serializers
 class UserSettingSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserSetting
-        fields = ["nickname", "major", "type", "enlisted_date", "delisted_date", "promotion1_date", "promotion2_date", "promotion3_date"]
+        fields = ["email", "nickname", "major", "type", "enlisted_date", "delisted_date", "promotion1_date", "promotion2_date", "promotion3_date"]
+        extra_kwargs = {"email": {"read_only": True}}
+    def validate_nickname(val):
+        if len(val) < 4 or len(val) > 15 :
+            raise serializers.ValidationError({'nickname' : '닉네임은 4자 이상 15자 이하여야 합니다.'})
+        return val
         
