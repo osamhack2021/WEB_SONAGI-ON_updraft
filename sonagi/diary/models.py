02fd26b448dc9e0262baefb18843b37631ed7d19
kookhh0827 <kookhh0827@gmail.com>
2021-10-16 12:57:42 +0000

    modify diary write_date editable for admin

--- sonagi/diary/models.py
+++ sonagi/diary/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -19,5 +20,5 @@ class Diary(models.Model):
     title = models.CharField(verbose_name="한줄평", max_length=100, null=False)
     content = models.CharField(verbose_name="내용", max_length=2000, null=True)
     emotion = models.CharField(verbose_name="감정", max_length=10, choices=emotion_choices, null=False)
-    write_date = models.DateField(verbose_name="작성 일자", auto_now_add=True)
+    write_date = models.DateField(verbose_name="작성 일자", default=timezone.now)
     rewrite_date = models.DateField(verbose_name="최종 수정 일자", auto_now=True)
