34ec8d49e8de6951f895be1c0933ff660466b082
kookhh0827 <kookhh0827@gmail.com>
2021-10-17 05:11:42 +0000

    now you can access static image files

--- sonagi/sonagi/urls.py
+++ sonagi/sonagi/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +32,4 @@ urlpatterns = [
     path("api/diary/", include("diary.urls")),
     # contest app path
     path("api/contest/", include("contest.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
