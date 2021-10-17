90ad62b89ffc652312102e79761036eace8b36b5
kookhh0827 <kookhh0827@gmail.com>
2021-10-17 04:41:00 +0000

    add multipart parser

--- sonagi/sonagi/settings.py
+++ sonagi/sonagi/settings.py
@@ -184,6 +184,7 @@ SITE_ID = 1
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.MultiPartParser'
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         #'rest_framework.authentication.TokenAuthentication',
