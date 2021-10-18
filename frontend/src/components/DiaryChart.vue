beaf3592754e301ab3899a5732c6e89cbbde0cc6
kookhh0827 <kookhh0827@gmail.com>
2021-10-18 11:21:10 +0000

    modify dashboard chart

--- frontend/src/components/DiaryChart.vue
+++ frontend/src/components/DiaryChart.vue
@@ -23,10 +23,17 @@ export default {
   data: () => ({
     isLoaded: false,
     chartdata: {"labels":[], "datasets": [{"label":"","borderColor": '#FC2525', "data":[]}]},    
-    value: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     options: {
       legend: {
           display: false
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: 30
+          }
+        }],
       },
     }
   }),
@@ -35,16 +42,16 @@ export default {
   },
   methods: {
     updateChart: function(){
+      this.chartdata.labels = [];
+      let value = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
       let enlisted_date = new Date(this.userdata.enlisted_date);
       let delisted_date = new Date(this.userdata.delisted_date);
       enlisted_date.setDate(1);
       delisted_date.setDate(1);
-      this.chartdata.labels = ["입대"];
-      while(enlisted_date < delisted_date){
+      while(enlisted_date <= delisted_date){
+        this.chartdata.labels.push(enlisted_date.getFullYear()+"-"+(enlisted_date.getMonth()+1));
         enlisted_date.setMonth(enlisted_date.getMonth() + 1);
-        this.chartdata.labels.push((enlisted_date.getMonth()+1)+"월");
       }
-      this.chartdata.labels[this.chartdata.labels.length-1] = "전역";
       const config = {
         headers: {
           Authorization: `Bearer ${this.access_token}`,
@@ -55,11 +62,17 @@ export default {
         .then((res) => {
           for(let i = 0; i < res.data.length; i++){
             let tmp = new Date(res.data[i]['write_date']);
-            tmp = (tmp.getMonth()+1)+"월";
+            tmp = tmp.getFullYear()+"-"+(tmp.getMonth()+1);
             let idx = this.chartdata.labels.findIndex((val) => val.includes(tmp));
-            this.value[idx] += 1;
+            value[idx] += 1;
           }
-          this.chartdata.datasets[0].data= this.value;
+          for(let i = 0; i < this.chartdata.labels.length; i++){
+            this.chartdata.labels[i] = this.chartdata.labels[i].split('-')[1]+"월";
+          }
+          this.chartdata.labels[0] = "입대";
+          this.chartdata.labels[this.chartdata.labels.length-1] = "전역";
+          console.log(this.chartdata.labels);
+          this.chartdata.datasets[0].data= value;
           this.isLoaded = true;
         })
     }
